28ccae47531c5237c03cd2faa4316c1646deb047
Derick Saaa <dmsaao@unal.edu.co>
2022-12-23 13:38:35 -0500

    estilos de carrito listos

--- src/CartShop.jsx
+++ src/CartShop.jsx
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import Delete from './images/delete.png'
 import kabuki from './images/kabuki.png';
 import  './CartShop.css';
+import { NavLink } from 'react-router-dom';
 
 
 const CartShop = () => {
@@ -17,6 +18,7 @@ const CartShop = () => {
   }
 
   return (
+    <div className='shop'>
   <div className='cartshop'>
 
     <div>
@@ -40,6 +42,15 @@ const CartShop = () => {
       </div>
     </div>
 
+  </div>
+  <div className='resume'>
+    <h2>Resumen</h2>
+    <h3>total:     45000cop</h3>
+    <NavLink to='/MakeUp'><button>Finalizar compra</button>
+    </NavLink>
+  </div>
+  <NavLink to='/MakeUp'><button className='lastbutton'>Seguir comprando</button>
+  </NavLink>
   </div>
   )
 }
