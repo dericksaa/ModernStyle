6bfeba720dfcf9ca8a25a43837789b103cca1988
Derick Saaa <dmsaao@unal.edu.co>
2022-12-24 19:32:58 -0500

    añadiendo el componente cartproduct

---
+++ src/CartProduct.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {useState} from 'react'
+import Delete from './images/delete.png'
+import kabuki from './images/kabuki.png';
+
+export const CartProduct = () => {
+
+  const [cantidad, setCantidad]=useState(1)
+
+  const sumador =(e)=>{
+    setCantidad(cantidad+1);
+  }
+  const restart =(e)=>{
+      (cantidad > 0) && setCantidad(cantidad-1);   
+  }
+
+
+return (
+  <div className='cartshop'>
+
+  <div>
+    <h3>Producto:</h3>
+    <img src={kabuki} alt="" />
+  </div>
+
+  <div>
+    <h3>Kit Kabuki</h3>
+    <h3>Precio:</h3>
+    <h3>Cantidad:</h3>
+  </div>
+
+  <div>
+    <img src={Delete} alt="Icono de borrado" />
+    <h3>15000 cop</h3>
+    <div className='contador'>
+      <button onClick={sumador}>+</button>
+      <h3>{cantidad}</h3>
+      <button onClick={restart}>-</button>
+    </div>
+  </div>
+
+</div>
+)
+}
--- src/CartShop.jsx
+++ src/CartShop.jsx
@@ -4,53 +4,23 @@ import Delete from './images/delete.png'
 import kabuki from './images/kabuki.png';
 import  './CartShop.css';
 import { NavLink } from 'react-router-dom';
+import { CartProduct } from './CartProduct';
 
 
 const CartShop = () => {
 
-  const [cantidad, setCantidad]=useState(1)
-
-  const sumador =(e)=>{
-    setCantidad(cantidad+1);
-  }
-  const restart =(e)=>{
-      (cantidad > 0) && setCantidad(cantidad-1);   
-  }
 
   return (
-    <div className='shop'>
-  <div className='cartshop'>
-
-    <div>
-      <h3>Producto:</h3>
-      <img src={kabuki} alt="" />
+  <div className='shop'>
+    <CartProduct/>
+    <div className='resume'>
+      <h2>Resumen</h2>
+      <h3>total:     45000cop</h3>
+      <NavLink to='/MakeUp'><button>Finalizar compra</button>
+      </NavLink>
     </div>
-
-    <div>
-      <h3>Kit Kabuki</h3>
-      <h3>Precio:</h3>
-      <h3>Cantidad:</h3>
-    </div>
-
-    <div>
-      <img src={Delete} alt="Icono de borrado" />
-      <h3>15000 cop</h3>
-      <div className='contador'>
-        <button onClick={sumador}>+</button>
-        <h3>{cantidad}</h3>
-        <button onClick={restart}>-</button>
-      </div>
-    </div>
-
-  </div>
-  <div className='resume'>
-    <h2>Resumen</h2>
-    <h3>total:     45000cop</h3>
-    <NavLink to='/MakeUp'><button>Finalizar compra</button>
+    <NavLink to='/MakeUp'><button className='lastbutton'>Seguir comprando</button>
     </NavLink>
-  </div>
-  <NavLink to='/MakeUp'><button className='lastbutton'>Seguir comprando</button>
-  </NavLink>
   </div>
   )
 }
