86d641a4dac164f623dd581b89986fca7dd97132
Derick Saaa <dmsaao@unal.edu.co>
2022-12-30 18:22:53 -0500

    enlaces a redes sociales

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,16 +4,17 @@ import insta from './images/instagram.png'
 import tiktok from './images/tiktok.png'
 
 
+
 const Footer =()=>{
     return(
         <div className='footer'>
             <div className='social'>
-                <img src={facebook} alt="fk" />
-                <img src={insta} alt="ig" />
-                <img src={tiktok} alt="tk" />
+                <a target='_blank' href="https://www.facebook.com/ModernStyleCosmetics"><img src={facebook} alt="fk" /> </a>  
+                <a target='_blank' href="https://www.instagram.com/modernstyle_makeup/?hl=es-la"><img src={insta} alt="ig" /> </a>  
+                <a target='_blank' href="https://www.tiktok.com/@modern_style0?_t=8V3j0InomEY&_r=1"><img src={tiktok} alt="tk" /> </a> 
             </div>
             <h3>Modern Style</h3>
-            <h6>Developed by Saa</h6>
+            <h6>Developed by D.S.</h6>
         </div>
     )
 }
